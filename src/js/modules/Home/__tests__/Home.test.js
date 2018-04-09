d3c1d4da7f44622d6b6461631747bcbb99f053a6
Christian Freitas <chrfreitas@gmail.com>
2018-04-09 14:31:08 -0300

    Unit test to home

--- src/js/modules/Home/__tests__/Home.test.js
+++ src/js/modules/Home/__tests__/Home.test.js
@@ -27,4 +27,20 @@ describe('Home Module', () => {
         wrapper.instance().toggleModal(domain);
         expect(wrapper.state().modal.opened).toBeTruthy();
     })
+
+    it('should update the domain object with the new object', () => {
+        const domain = {
+            id: 2,
+            name: 'lalala',
+            email: 'lalala@gmail.com',
+            price: '$3.00'
+        }
+
+        const wrapper = shallow(<Home />);
+        wrapper.instance().update(domain);
+
+        const newPrice = wrapper.state().domains.find(item => item.id === domain.id).price;
+
+        expect(newPrice).toBe(domain.price);
+    })
 });
