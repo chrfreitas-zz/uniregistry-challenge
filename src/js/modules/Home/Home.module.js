44a27bc9b47ede3aee216ba6446c62a9b00ac7ca
Christian Freitas <chrfreitas@gmail.com>
2018-04-10 10:19:26 -0300

    Base animation

--- src/js/modules/Home/Home.module.js
+++ src/js/modules/Home/Home.module.js
@@ -58,18 +58,26 @@ class Home extends Component {
         this.setState({ editMode: option });
     }
 
+    getClassCol() {
+        if(this.state.editMode){
+            return 'col is-editing';
+        }
+
+        return 'col';
+    }
+
     render() {
         return (
             <div className="container">
                 <div className="row">
-                    <div className="col">
-                        <div className="row">
+                    <div className={this.getClassCol()}>
+                        <div className="row domain-table">
                             <div className="col-12">
                                 <DomainsTable domains={this.state.domains} goEdit={this.goEdit}/>
                             </div>
                         </div>
 
-                        <div className="row justify-content-center">
+                        <div className="row justify-content-center domain-form">
                             <div className="col-6">
                                 { this.state.editMode && <DomainForm domain={this.state.selectedDomain} update={this.update} setEditMode={this.setEditMode} /> }
                             </div>
